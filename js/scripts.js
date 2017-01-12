c95ca83cb3ed335ae546bf110f9a8ccde045671e
brendan grubb <brendangrubb@gmail.com>
2017-01-12 14:39:10 -0800

    change layout to accomodate selector box

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,12 @@
 $(document).ready(function() {
 
-  $("button#snake-btn").click(function() {
-    $(".text").fadeOut(0)
-    $("#snake-info").toggle(1000);
-    $("#outro").show();
-  })
+  var animal = $("#animal").val();
+  var snake = $("#snake-info").val();
+  var turtle = $("#turtle-info").val();
+  var insect = $("insect").val();
 
-  $("button#turtle-btn").click(function() {
-    $(".text").fadeOut(0)
-    $("#turtle-info").toggle(1000);
-    $("#outro").show();
-  })
+  if (animal = insect) {
+    $("#insect-info").show();
+  }
 
-  $("button#insect-btn").click(function() {
-    $(".text").fadeOut(0)
-    $("#insect-info").toggle(1000);
-    $("#outro").show();
-  })
 });
